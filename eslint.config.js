a681d12a41b9c3104c88cf9b6c1f9e019856404f
RoshenR <crisliath@gmail.com>
2025-12-21 19:06:44 +0100

    Modified things

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+// eslint.config.js
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+    js.configs.recommended,
+    {
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+            globals: {
+                ...globals.node,
+                ...globals.jest, // si tu utilises jest
+            },
+        },
+        rules: {
+            "no-unused-vars": "warn",
+            "no-console": "off",
+        },
+    },
+];
