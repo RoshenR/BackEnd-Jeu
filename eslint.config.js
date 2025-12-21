73169fedc9cf15fe922bb29c532b6bda5033211d
RoshenR <crisliath@gmail.com>
2025-12-21 19:17:37 +0100

    Modified things

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,9 @@
+// eslint.config.js
 import js from "@eslint/js";
 import globals from "globals";
 
 export default [
+    // Base (par défaut: Node)
     js.configs.recommended,
     {
         languageOptions: {
@@ -12,8 +14,29 @@ export default [
             },
         },
         rules: {
-            "no-unused-vars": "warn",
+            // tu peux garder strict, mais je te mets le minimum
             "no-console": "off",
+            "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+        },
+    },
+
+    // Front: autoriser document/window/etc.
+    {
+        files: ["front/**/*.js"],
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+            },
+        },
+    },
+
+    // Tests Mocha: autoriser describe/it/beforeEach/afterEach...
+    {
+        files: ["tests/**/*.js", "tests/**/*.test.js"],
+        languageOptions: {
+            globals: {
+                ...globals.mocha,
+            },
         },
     },
 ];
